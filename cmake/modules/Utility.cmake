049b097aba77d6922493faf1eea3260afc6b251d
Daniel Dunbar <daniel_dunbar@apple.com>
2014-09-17 21:48:28 +0000

    Add a CMake based build system.

---
+++ cmake/modules/Utility.cmake
@@ -0,0 +1,7 @@
+function(append_if condition value)
+  if (${condition})
+    foreach(variable ${ARGN})
+      set(${variable} "${${variable}} ${value}" PARENT_SCOPE)
+    endforeach(variable)
+  endif()
+endfunction()
