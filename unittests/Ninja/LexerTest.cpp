fa85d904703c9c5974d8b4fb6e0535172603f525
Daniel Dunbar <daniel_dunbar@apple.com>
2014-09-18 23:07:27 +0000

    [Ninja] Finish basic ninja::Lexer unites.

--- unittests/Ninja/LexerTest.cpp
+++ unittests/Ninja/LexerTest.cpp
@@ -20,14 +20,58 @@ namespace {
 
 TEST(LexerTest, Basic) {
   char Input[] = "| : || # Comment";
-  ninja::Lexer Lexer(Input, strlen(Input));
+  size_t InputSize = strlen(Input);
+  ninja::Lexer Lexer(Input, InputSize);
 
   // Check that we get the appropriate tokens.
   ninja::Token Tok;
+  ninja::Token &Result = Lexer.lex(Tok);
+
+  // Check .lex() result.
+  EXPECT_EQ(&Result, &Tok);
+
+  // Check first token.
+  EXPECT_EQ(ninja::Token::Kind::Pipe, Tok.TokenKind);
+  EXPECT_EQ(&Input[0], Tok.Start);
+  EXPECT_EQ(1U, Tok.Length);
+  EXPECT_EQ(1U, Tok.Line);
+  EXPECT_EQ(0U, Tok.Column);
+
+  // Check second token.
   Lexer.lex(Tok);
-  
-  Tok.dump();
-  EXPECT_TRUE(false);
+  EXPECT_EQ(ninja::Token::Kind::Colon, Tok.TokenKind);
+  EXPECT_EQ(&Input[2], Tok.Start);
+  EXPECT_EQ(1U, Tok.Length);
+  EXPECT_EQ(1U, Tok.Line);
+  EXPECT_EQ(2U, Tok.Column);
+
+  // Check third token.
+  Lexer.lex(Tok);
+  EXPECT_EQ(ninja::Token::Kind::PipePipe, Tok.TokenKind);
+  EXPECT_EQ(&Input[4], Tok.Start);
+  EXPECT_EQ(2U, Tok.Length);
+  EXPECT_EQ(1U, Tok.Line);
+  EXPECT_EQ(4U, Tok.Column);
+
+  // Check fourth token.
+  Lexer.lex(Tok);
+  EXPECT_EQ(ninja::Token::Kind::Comment, Tok.TokenKind);
+  EXPECT_EQ(&Input[7], Tok.Start);
+  EXPECT_EQ(9U, Tok.Length);
+  EXPECT_EQ(1U, Tok.Line);
+  EXPECT_EQ(7U, Tok.Column);
+
+  // Check final token.
+  Lexer.lex(Tok);
+  EXPECT_EQ(ninja::Token::Kind::EndOfFile, Tok.TokenKind);
+  EXPECT_EQ(&Input[strlen(Input)], Tok.Start);
+  EXPECT_EQ(0U, Tok.Length);
+  EXPECT_EQ(1U, Tok.Line);
+  EXPECT_EQ(InputSize, Tok.Column);
+
+  // Check we continue to get EOF.
+  Lexer.lex(Tok);
+  EXPECT_EQ(ninja::Token::Kind::EndOfFile, Tok.TokenKind);
 }
 
 }
