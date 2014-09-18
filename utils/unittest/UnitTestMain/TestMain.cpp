9c53ab827cc3854518e1f6b3238ee70883b45fba
Daniel Dunbar <daniel_dunbar@apple.com>
2014-09-18 22:34:49 +0000

    [unittests] Import googletest, a la LLVM.
     - I removed the raw_ostream support, since we don’t have it available
       (yet), but otherwise this is just a copy from LLVM.

---
+++ utils/unittest/UnitTestMain/TestMain.cpp
@@ -0,0 +1,16 @@
+//===--- utils/unittest/UnitTestMain/TestMain.cpp -------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
