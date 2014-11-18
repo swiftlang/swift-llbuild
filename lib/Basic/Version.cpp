2ebd2420429e116e8b323c7726a66a521f6ab6af
Daniel Dunbar <daniel_dunbar@apple.com>
2014-11-18 14:38:43 -0800

    [build] Add B&I project version to —version, if available.

--- lib/Basic/Version.cpp
+++ lib/Basic/Version.cpp
@@ -17,7 +17,11 @@
 namespace llbuild {
 
 std::string getLLBuildFullVersion() {
+#ifdef LLBUILD_VERSION_STRING
+    return "LLBuild version 0.1 (" + std::string(LLBUILD_VERSION_STRING) + ")";
+#else
     return "LLBuild version 0.1";
+#endif
 }
 
 }
