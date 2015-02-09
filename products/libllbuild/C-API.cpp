7454a836540eb09bdc483cf401f26cd228c726bb
Daniel Dunbar <daniel_dunbar@apple.com>
2015-02-08 17:06:39 -0800

    [libllbuild] Add initial C API to BuildEngine.
     - Currently just a naive translation of the C++ APIs into C, and not all APIs
       are made available yet.

--- products/libllbuild/C-API.cpp
+++ products/libllbuild/C-API.cpp
@@ -14,12 +14,133 @@
 #include <llbuild/llbuild.h>
 
 #include "llbuild/Basic/Version.h"
+#include "llbuild/Core/BuildEngine.h"
 
 using namespace llbuild;
+using namespace llbuild::core;
+
+/* Misc API */
 
 const char* llb_get_full_version_string(void) {
   // Use a static local to store the version string, to avoid lifetime issues.
   static std::string VersionString = getLLBuildFullVersion();
 
   return VersionString.c_str();
+}
+
+/* Build Engine API */
+
+namespace {
+
+class CAPIBuildEngineDelegate : public BuildEngineDelegate {
+  llb_buildengine_delegate_t CAPIDelegate;
+
+  virtual ~CAPIBuildEngineDelegate() {
+    if (CAPIDelegate.destroy_context) {
+      CAPIDelegate.destroy_context(CAPIDelegate.context);
+    }
+  }
+
+  virtual Rule lookupRule(const KeyType& Key) override {
+    llb_rule_t rule;
+    llb_data_t key_data{ Key.length(), (const uint8_t*)Key.data() };
+    CAPIDelegate.lookup_rule(CAPIDelegate.context, &key_data, &rule);
+
+    std::function<bool(const Rule&, const ValueType&)> IsResultValid;
+    if (rule.is_result_valid) {
+      IsResultValid = [rule] (const Rule& NativeRule, const ValueType& Value) {
+        // FIXME: Why do we pass the rule here, it is redundant. NativeRule
+        // should be == rule here.
+        llb_data_t value_data{ Value.size(), Value.data() };
+        return rule.is_result_valid(rule.context, &rule, &value_data);
+      };
+    }
+
+    return Rule{
+      KeyType((const char*)rule.key.data, rule.key.length),
+      [rule] (BuildEngine& engine) {
+        return (Task*) rule.create_task(
+          rule.context, (llb_buildengine_t*) &engine);
+      },
+      IsResultValid };
+  }
+
+public:
+  CAPIBuildEngineDelegate(llb_buildengine_delegate_t delegate)
+    : CAPIDelegate(delegate)
+  {
+    
+  }
+};
+
+class CAPITask : public Task {
+  llb_task_delegate_t CAPIDelegate;
+
+public:
+  CAPITask(llb_task_delegate_t delegate)
+    : Task("C-API-Task"), CAPIDelegate(delegate)
+  {
+    
+  }
+
+  virtual void start(BuildEngine& Engine) override {
+    CAPIDelegate.start(CAPIDelegate.context, (llb_task_t*)this,
+                       (llb_buildengine_t*) &Engine);
+  }
+
+  virtual void provideValue(BuildEngine& Engine, uintptr_t InputID,
+                            const ValueType& Value) override {
+    llb_data_t ValueData{ Value.size(), Value.data() };
+    CAPIDelegate.provide_value(CAPIDelegate.context, (llb_task_t*)this,
+                               (llb_buildengine_t*) &Engine,
+                               InputID, &ValueData);
+  }
+
+  virtual void inputsAvailable(BuildEngine& Engine) override {
+    CAPIDelegate.inputs_available(CAPIDelegate.context, (llb_task_t*)this,
+                                  (llb_buildengine_t*) &Engine);
+  }
+};
+
+};
+
+llb_buildengine_t* llb_buildengine_create(llb_buildengine_delegate_t delegate) {
+  // FIXME: Delegate is leaked, need to provide a provision for owning the
+  // delegate.
+  BuildEngineDelegate* Delegate = new CAPIBuildEngineDelegate(delegate);
+  return (llb_buildengine_t*) new BuildEngine(*Delegate);
+}
+
+void llb_buildengine_destroy(llb_buildengine_t* engine) {
+  // FIXME: Delegate is lost.
+  delete (BuildEngine*)engine;
+}
+
+void llb_buildengine_build(llb_buildengine_t* engine_p, const llb_data_t* key,
+                           llb_data_t* result_out) {
+  BuildEngine* Engine = (BuildEngine*) engine_p;
+
+  auto Result = Engine->build(KeyType((const char*)key->data, key->length));
+
+  *result_out = llb_data_t{ Result.size(), Result.data() };
+}
+
+llb_task_t* llb_buildengine_register_task(llb_buildengine_t* engine_p,
+                                          llb_task_t* task) {
+  BuildEngine* Engine = (BuildEngine*) engine_p;
+  Engine->registerTask((Task*)task);
+  return task;
+}
+
+void llb_buildengine_task_is_complete(llb_buildengine_t* engine_p,
+                                      llb_task_t* task,
+                                      const llb_data_t* value) {
+  BuildEngine* Engine = (BuildEngine*) engine_p;
+  std::vector<uint8_t> Result(value->length);
+  memcpy(Result.data(), value->data, value->length);
+  Engine->taskIsComplete((Task*)task, std::move(Result));
+}
+
+llb_task_t* llb_task_create(llb_task_delegate_t delegate) {
+  return (llb_task_t*) new CAPITask(delegate);
 }
--- products/libllbuild/public-api/llbuild/llbuild.h
+++ products/libllbuild/public-api/llbuild/llbuild.h
@@ -17,6 +17,9 @@
 #ifndef LLBUILD_PUBLIC_H
 #define LLBUILD_PUBLIC_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #if defined(__cplusplus)
 #define LLBUILD_EXPORT extern "C"
 #elif __GNUC__
@@ -27,5 +30,123 @@
 
 /// Get the full version of the llbuild library.
 LLBUILD_EXPORT const char* llb_get_full_version_string(void);
+
+/// @name Build Engine
+///
+/// @{
+
+/// Opaque handle to a build engine.
+typedef struct llb_buildengine_t_ llb_buildengine_t;
+
+/// Opaque handle to an executing task.
+typedef struct llb_task_t_ llb_task_t;
+
+/// Representation for a blob of bytes.
+typedef struct llb_data_t_ {
+    uint64_t length;
+    const uint8_t* data;
+} llb_data_t;
+
+/// Rule representation.
+typedef struct llb_rule_t_ llb_rule_t;
+struct llb_rule_t_ {
+    /// User context pointer.
+    void* context;
+
+    /// The key this rule computes.
+    llb_data_t key;
+
+    /// The callback to create a task for computing this rule.
+    llb_task_t* (*create_task)(void* context,
+                               llb_buildengine_t* engine);
+
+    /// The callback to check if a previously computed result is still valid.
+    bool (*is_result_valid)(void* context, const llb_rule_t* rule,
+                            const llb_data_t* result);
+};
+
+/// Delegate structure for callbacks required by the build engine.
+typedef struct llb_buildengine_delegate_t_ {
+    /// User context pointer.
+    void* context;
+
+    /// Callback for releasing the user context, called on engine destruction.
+    void (*destroy_context)(void* context);
+
+    /// Callback for resolving keys to the rule that should be used to compute
+    /// them.
+    ///
+    /// Xparam context The user context pointer.
+    /// Xparam key The key being looked up.
+    /// Xparam rule_out [out] On return, the rule to use to build the given key.
+    void (*lookup_rule)(void* context,
+                        const llb_data_t* key,
+                        llb_rule_t* rule_out);
+} llb_buildengine_delegate_t;
+
+/// Create a new build engine object.
+///
+/// \param delegate The delegate to use for build engine operations.
+LLBUILD_EXPORT llb_buildengine_t*
+llb_buildengine_create(llb_buildengine_delegate_t delegate);
+
+/// Destroy a build engine.
+LLBUILD_EXPORT void
+llb_buildengine_destroy(llb_buildengine_t* engine);
+
+/// Build the result for a particular key.
+///
+/// \param engine The engine to operate on.
+/// \param key The key to build.
+/// \param result_out [out] On return, the result of computing the given key.
+LLBUILD_EXPORT void
+llb_buildengine_build(llb_buildengine_t* engine, const llb_data_t* key,
+                      llb_data_t* result_out);
+
+/// Register the given task, in response to a Rule evaluation.
+///
+/// The engine tasks ownership of the \arg task, and it is expected to
+/// subsequently be returned as the task to execute for a rule evaluation.
+///
+/// \returns The provided task, for the convenience of the client.
+LLBUILD_EXPORT llb_task_t*
+llb_buildengine_register_task(llb_buildengine_t* engine, llb_task_t* task);
+
+
+/// Called by a task to indicate it has completed and to provide its value.
+///
+/// It is legal to call this method from any thread.
+LLBUILD_EXPORT void
+llb_buildengine_task_is_complete(llb_buildengine_t* engine, llb_task_t* task,
+                                 const llb_data_t* value);
+
+/// @}
+
+/// @name Build Engine Task
+/// @{
+
+/// Delegate structure for callbacks required by a task.
+typedef struct llb_task_delegate_t_ {
+    /// User context pointer.
+    void* context;
+
+    /// The callback indicating the task has been started.
+    void (*start)(void* context, llb_task_t* task, llb_buildengine_t* engine);
+
+    /// The callback to provide a requested input value to the task.
+    void (*provide_value)(void* context, llb_task_t* task,
+                          llb_buildengine_t* engine, uintptr_t input_id,
+                          const llb_data_t* value);
+
+    /// The callback indicating that all requested inputs have been provided.
+    void (*inputs_available)(void* context, llb_task_t* task,
+                             llb_buildengine_t* engine);
+} llb_task_delegate_t;
+
+/// Create a task object.
+LLBUILD_EXPORT llb_task_t*
+llb_task_create(llb_task_delegate_t delegate);
+
+/// @}
 
 #endif
