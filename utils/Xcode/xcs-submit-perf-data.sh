b831a7084e3fc60b82af254d8d2eb0ce61bee063
Daniel Dunbar <daniel_dunbar@apple.com>
2015-07-30 20:51:01 -0700

    [utils] Report the llbuild git revision with performance data.

--- utils/Xcode/xcs-submit-perf-data.sh
+++ utils/Xcode/xcs-submit-perf-data.sh
@@ -7,11 +7,15 @@ echo "**** Computing Revision Index ****"
 num_commits=$(echo $(cd "${XCS_SOURCE_DIR}"/llbuild && git log --format=oneline | wc -l))
 echo "llbuild commits: ${num_commits}"
 
+# Get the current revision.
+revision=$(cd "${XCS_SOURCE_DIR}"/llbuild && git log -1 --format=oneline | cut -d\  -f1)
+
 # Extract the performance data into the LNT format.
 echo "**** Converting Performance Data ****"
 data_path="${TMPDIR}/performance-data.json"
 "${XCS_SOURCE_DIR}/llbuild/utils/Xcode/extract-perf-data" \
   --run-order "${num_commits}" \
+  --revision "${revision}" \
   --machine-name dtprojectcore \
   --output-path "${data_path}" \
   "${XCS_DERIVED_DATA_DIR}/Logs/Test/"*.xcactivitylog
