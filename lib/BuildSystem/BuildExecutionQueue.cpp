0afa350521a69a4d1852252d4ad2eaa24341dce2
Daniel Dunbar <daniel_dunbar@apple.com>
2016-01-12 11:43:35 -0800

    [BuildSystem] Move execution queue code to its own file.

---
+++ lib/BuildSystem/BuildExecutionQueue.cpp
@@ -0,0 +1,33 @@
+//===-- BuildExecutionQueue.cpp -------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#include "llbuild/BuildSystem/BuildExecutionQueue.h"
+
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/StringRef.h"
+
+#include <vector>
+
+using namespace llbuild;
+using namespace llbuild::buildsystem;
+
+BuildExecutionQueue::~BuildExecutionQueue() {
+}
+
+bool BuildExecutionQueue::executeShellCommand(QueueJobContext* context,
+                                              StringRef command) {
+  SmallString<1024> commandStorage(command);
+  std::vector<StringRef> commandLine(
+      { "/bin/sh", "-c", commandStorage.c_str() });
+  return executeProcess(context, commandLine);
+}
--- lib/BuildSystem/BuildSystem.cpp
+++ lib/BuildSystem/BuildSystem.cpp
@@ -40,16 +40,6 @@ using namespace llbuild::basic;
 using namespace llbuild::core;
 using namespace llbuild::buildsystem;
 
-BuildExecutionQueue::~BuildExecutionQueue() {}
-
-bool BuildExecutionQueue::executeShellCommand(QueueJobContext* context,
-                                              StringRef command) {
-  SmallString<1024> commandStorage(command);
-  std::vector<StringRef> commandLine(
-      { "/bin/sh", "-c", commandStorage.c_str() });
-  return executeProcess(context, commandLine);
-}
-
 BuildSystemDelegate::~BuildSystemDelegate() {}
 
 BuildSystemCommandInterface::~BuildSystemCommandInterface() {}
