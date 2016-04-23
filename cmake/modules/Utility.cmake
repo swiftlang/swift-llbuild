49091b0f8343769fae181bd5a7fddf4e125df0e4
Ankit Aggarwal <ankit.spd@gmail.com>
2016-04-23 14:36:18 +0530

    [swift-bindings] Compile swift bindings for OSX

--- cmake/modules/Utility.cmake
+++ cmake/modules/Utility.cmake
@@ -111,3 +111,98 @@ function(add_unittest test_suite test_name)
 
   set_property(TARGET ${test_name} APPEND PROPERTY COMPILE_DEFINITIONS GTEST_HAS_RTTI=0)
 endfunction()
+
+# Compile swift sources to a dynamic framework.
+# Usage:
+# target          # Target name
+# name            # Swift Module name
+# deps            # Target dependencies
+# sources         # List of sources
+# additional_args # List of additional args to pass
+function(add_swift_module target name deps sources additional_args)
+  
+  set(BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY})
+  
+  list(APPEND ARGS -module-name ${name})
+  list(APPEND ARGS -incremental -emit-dependencies -emit-module)
+  list(APPEND ARGS -emit-module-path ${name}.swiftmodule)
+  
+  set(FILEMAP ${BUILD_DIR}/output-file-map.json)
+  set(OUTPUT_FILE_MAP ${FILEMAP})
+  
+  # Remove old file and start writing new one.
+  file(REMOVE ${FILEMAP})
+  file(APPEND ${FILEMAP} "{\n")
+  foreach(source ${sources})
+    file(APPEND ${FILEMAP} "\"${CMAKE_CURRENT_SOURCE_DIR}/${source}\": {\n")
+    file(APPEND ${FILEMAP} "\"dependencies\": \"${BUILD_DIR}/${source}.d\",\n")
+    set(OBJECT ${BUILD_DIR}/${source}.o)
+    list(APPEND OUTPUTS ${OBJECT})
+    file(APPEND ${FILEMAP} "\"object\": \"${OBJECT}\",\n")
+    file(APPEND ${FILEMAP} "\"swiftmodule\": \"${BUILD_DIR}/${source}~partial.swiftmodule\",\n")
+    file(APPEND ${FILEMAP} "\"swift-dependencies\": \"${BUILD_DIR}/${source}.swiftdeps\"\n},\n")
+  endforeach()
+  file(APPEND ${FILEMAP} "\"\": {\n")
+  file(APPEND ${FILEMAP} "\"swift-dependencies\": \"${BUILD_DIR}/master.swiftdeps\"\n")
+  file(APPEND ${FILEMAP} "}\n")
+  file(APPEND ${FILEMAP} "}")
+    
+  list(APPEND ARGS -output-file-map ${OUTPUT_FILE_MAP})
+  list(APPEND ARGS -parse-as-library)
+  list(APPEND ARGS -c)
+
+  foreach(source ${sources})
+      list(APPEND ARGS ${CMAKE_CURRENT_SOURCE_DIR}/${source})
+  endforeach()
+
+  # FIXME: Find a better way to handle build types.
+  if (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+    list(APPEND ARGS -g)
+  endif()
+  if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    list(APPEND ARGS -Onone -g)    
+  else()
+    list(APPEND ARGS -O -whole-module-optimization)
+  endif()
+
+  foreach(arg ${additional_args})
+    list(APPEND ARGS ${arg})
+  endforeach()
+  
+  if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    list(APPEND ARGS -sdk ${CMAKE_OSX_SYSROOT})
+  endif()
+  
+  # Compile swiftmodule.
+  add_custom_command(
+      OUTPUT    ${OUTPUTS}
+      COMMAND   swiftc
+      ARGS      ${ARGS}
+      DEPENDS   ${sources}
+  )
+  
+  # Link and create dynamic framework.
+  set(DYLIB_OUTPUT ${LLBUILD_LIBRARY_OUTPUT_INTDIR}/${target}.dylib)
+  
+  if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    list(APPEND DYLYB_ARGS -sdk ${CMAKE_OSX_SYSROOT})
+  endif()
+  
+  list(APPEND DYLYB_ARGS -module-name ${name})
+  list(APPEND DYLYB_ARGS -o ${DYLIB_OUTPUT})
+  list(APPEND DYLYB_ARGS -emit-library ${OUTPUTS})
+  foreach(arg ${additional_args})
+    list(APPEND DYLYB_ARGS ${arg})
+  endforeach()
+  list(APPEND DYLYB_ARGS -L ${LLBUILD_LIBRARY_OUTPUT_INTDIR})
+  
+  add_custom_command(
+      OUTPUT    ${DYLIB_OUTPUT}
+      COMMAND   swiftc
+      ARGS      ${DYLYB_ARGS}
+      DEPENDS   ${OUTPUTS}
+  )
+  
+  # Add the target.    
+  add_custom_target(${target} ALL DEPENDS ${deps} ${DYLIB_OUTPUT} ${sources})
+endfunction()
