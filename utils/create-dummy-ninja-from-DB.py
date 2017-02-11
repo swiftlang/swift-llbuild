0f63f9fce1150ddb8994b34653f5363c3db8b7f4
Daniel Dunbar <daniel_dunbar@apple.com>
2017-02-10 16:47:27 -0800

    [utils] Update create-dummy-ninja-from-DB util.

--- utils/create-dummy-ninja-from-DB.py
+++ utils/create-dummy-ninja-from-DB.py
@@ -12,21 +12,34 @@ import argparse
 import sqlalchemy
 import sqlalchemy.ext.declarative
 from sqlalchemy import *
+from sqlalchemy.orm import *
 
 # DB Declaration
 
 Base = sqlalchemy.ext.declarative.declarative_base()
 
+class KeyName(Base):
+    __tablename__ = "key_names"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column('key', String, nullable=False)
+
+    def __repr__(self):
+        return "%s%r" % (
+            self.__class__.__name__, (self.id, self.name))
+
 class RuleResult(Base):
     __tablename__ = "rule_results"
 
     id = Column(Integer, nullable=False, primary_key=True)
-    key = Column(String, nullable=False)
+    key_id = Column(Integer, ForeignKey(KeyName.id),
+                     nullable=False)
     value = Column(Integer, nullable=False)
     built_at = Column(Integer, nullable=False)
     computed_at = Column(Integer, nullable=False)
 
-    dependencies = sqlalchemy.orm.relationship('RuleDependency')
+    key = relation(KeyName)
+    dependencies = relationship('RuleDependency')
 
     def __repr__(self):
         return "%s%r" % (
@@ -36,14 +49,17 @@ class RuleResult(Base):
 class RuleDependency(Base):
     __tablename__ = "rule_dependencies"
 
-    id = Column(Integer, nullable=False, primary_key=True)
     rule_id = Column(Integer, ForeignKey(RuleResult.id),
-                     nullable=False)
-    key = Column(String, nullable=False)
+                     nullable=False, primary_key=True)
+    key_id = Column(Integer, ForeignKey(KeyName.id),
+                     nullable=False, primary_key=True)
 
+    rule = relation(RuleResult)
+    key = relation(KeyName)
+    
     def __repr__(self):
         return "%s%r" % (
-            self.__class__.__name__, (self.id, self.rule_id, self.key))
+            self.__class__.__name__, (self.rule_id, self.key))
 
 def main():
     parser = argparse.ArgumentParser()
