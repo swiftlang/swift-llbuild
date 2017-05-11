swiftlang/swift-llbuild

commit bcc3d1011eff6450d74f9b0bec913d17bf48efa2

[Core] Ensure database maintains the relative order of dependencies.
 - As currently modeled, the engine only scans dependencies in serial order
   because we cannot yet handle the situation where a dependency has been
   inferred to be out-of-date, but it isn't yet at the head of the queue of
   things to actually begin to run in order to bring another node up-to-date.

 - Given that, it is very important that we do the scanning of dependencies in
   the order that they were declared, since there may be an implicit ordering
   relationship between the impact of evaluating those nodes. See the test case
   for an example (which also serves as a test case for the previous commit).

 - <rdar://problem/31490515> Serial dependency scanning is out-of-order

lib/Core/SQLiteBuildDB.cpp
@@ -32,7 +32,11 @@ using namespace llbuild::core;
 namespace {
 
 class SQLiteBuildDB : public BuildDB {
-  static const int currentSchemaVersion = 5;
+  /// Version History:
+  /// * 6: Added `ordinal` field for dependencies.
+  /// * 5: Switched to using `WITHOUT ROWID` for dependencies.
+  /// * 4: Pre-history
+  static const int currentSchemaVersion = 6;
 
   sqlite3 *db = nullptr;
 
@@ -165,10 +169,15 @@ public:
         // key. This allows us to use the table itself to perform efficient
         // queries for all of the keys associated with a particular rule_id, and
         // by doing so avoid having an ancillary index with duplicate data.
+        //
+        // We do, however, have to include an extra integer to identify the
+        // relative ordering of the dependencies, which the database is required
+        // to preserve.
         result = sqlite3_exec(
           db, ("CREATE TABLE rule_dependencies ("
                "rule_id INTEGER, "
                "key_id INTEGER, "
+               "ordinal INTEGER NONNULL, "
                "PRIMARY KEY (rule_id, key_id) "
                "FOREIGN KEY(rule_id) REFERENCES rule_results(id) "
                "FOREIGN KEY(key_id) REFERENCES key_names(id)) "
@@ -336,7 +345,8 @@ public:
 
   static constexpr const char *findRuleDependenciesStmtSQL =
   "SELECT key FROM key_names INNER JOIN rule_dependencies "
-  "ON key_names.id = rule_dependencies.key_id WHERE rule_id == ?;";
+  "ON key_names.id = rule_dependencies.key_id WHERE rule_id == ?"
+  "ORDER BY rule_dependencies.ordinal;";
   sqlite3_stmt* findRuleDependenciesStmt = nullptr;
 
   virtual bool lookupRuleResult(const Rule& rule, Result* result_out, std::string *error_out) override {
@@ -417,7 +427,8 @@ public:
   /// there is a duplicate dependencies for a rule, because the primary key is a
   /// composite of (rule_id, key).
   static constexpr const char *insertIntoRuleDependenciesStmtSQL =
-    "INSERT OR IGNORE INTO rule_dependencies(rule_id, key_id) VALUES (?, ?);";
+    "INSERT OR IGNORE INTO rule_dependencies(rule_id, key_id, ordinal) "
+    "VALUES (?, ?, ?);";
   sqlite3_stmt* insertIntoRuleDependenciesStmt = nullptr;
 
   static constexpr const char *deleteFromRuleResultsStmtSQL =
@@ -573,14 +584,21 @@ public:
     ruleID = sqlite3_last_insert_rowid(db);
 
     // Insert all the dependencies.
-    for (auto& dependency: ruleResult.dependencies) {
+    for (unsigned i = 0; i != ruleResult.dependencies.size(); ++i) {
+      const auto& dependency = ruleResult.dependencies[i];
+
+      // Reset the insert statement.
       result = sqlite3_reset(insertIntoRuleDependenciesStmt);
       assert(result == SQLITE_OK);
       result = sqlite3_clear_bindings(insertIntoRuleDependenciesStmt);
       assert(result == SQLITE_OK);
+
+      // Bind the rule ID.
       result = sqlite3_bind_int64(insertIntoRuleDependenciesStmt, /*index=*/1,
                                   ruleID);
       assert(result == SQLITE_OK);
+
+      // Bind the dependency key ID.
       uint64_t dependencyKeyID = getOrInsertKey(dependency, error_out);
       if (dependencyKeyID == UINT64_MAX) {
         return false;
@@ -588,6 +606,12 @@ public:
       result = sqlite3_bind_int64(insertIntoRuleDependenciesStmt, /*index=*/2,
                                  dependencyKeyID);
       assert(result == SQLITE_OK);
+
+      // Bind the index of the dependency.
+      result = sqlite3_bind_int(insertIntoRuleDependenciesStmt, /*index=*/3, i);
+      assert(result == SQLITE_OK);
+
+      // Perform the insert.
       result = sqlite3_step(insertIntoRuleDependenciesStmt);
       if (result != SQLITE_DONE) {
         *error_out = getCurrentErrorMessage();

include/llbuild/Core/BuildDB.h
@@ -61,6 +61,9 @@ public:
   /// unique. However, duplicate dependencies have no semantic meaning for the
   /// engine, and the database may elect to discard them from storage.
   ///
+  /// The database *MUST*, however, correctly maintain the order of the
+  /// dependencies.
+  ///
   /// \param error_out [out] Error string if return value is false.
   virtual bool setRuleResult(const Rule& rule, const Result& result, std::string* error_out) = 0;
 

tests/BuildSystem/Build/dep-scan-order.llbuild
@@ -1,0 +1,87 @@
+# Check that dependency scanning order is respected.
+#
+# RUN: rm -rf %t.build
+# RUN: mkdir -p %t.build
+# RUN: echo "input" > %t.build/input
+# RUN: cp %s %t.build/build.llbuild
+
+
+# Run an initial build forces "generated-input" to be one of the first declared
+# keys. This is explicitly checking against an implementation detail that, when
+# written, the dependency scanning order could be tied to the database key
+# identifier index.
+#
+# RUN: %{llbuild} buildsystem build --serial --chdir %t.build PREPARE > %t.prepare.out 2> %t.prepare.err || true
+
+
+# Run the actual initial build.
+#
+# RUN: %{llbuild} buildsystem build --serial --chdir %t.build > %t.out
+# RUN: %{FileCheck} --input-file=%t.out %s --check-prefix=CHECK-INITIAL
+# RUN: diff %t.build/input %t.build/generated-input
+# RUN: diff %t.build/input %t.build/output
+#
+# CHECK-INITIAL: CONSUMER
+
+
+# Check that a null build does nothing.
+#
+# RUN: %{llbuild} buildsystem build --serial --chdir %t.build > %t2.out
+# RUN: echo "END-OF-FILE" >> %t2.out
+# RUN: %{FileCheck} --input-file=%t2.out %s --check-prefix=CHECK-REBUILD
+#
+# CHECK-REBUILD-NOT: GENERATOR
+# CHECK-REBUILD-NOT: CONSUMER
+
+
+# Check that a modification triggers both uses.
+#
+# RUN: echo "MOD" >> %t.build/input
+# RUN: %{llbuild} buildsystem build --serial --chdir %t.build --trace /tmp/x.trace > %t3.out
+# RUN: %{FileCheck} --input-file=%t3.out %s --check-prefix=CHECK-AFTER-MOD
+# RUN: diff %t.build/input %t.build/generated-input
+# RUN: diff %t.build/input %t.build/output
+#
+# CHECK-AFTER-MOD: GENERATOR
+# CHECK-AFTER-MOD: CONSUMER
+
+
+# Check that another null build does nothing.
+#
+# RUN: %{llbuild} buildsystem build --serial --chdir %t.build > %t4.out
+# RUN: echo "END-OF-FILE" >> %t4.out
+# RUN: %{FileCheck} --input-file=%t4.out %s --check-prefix=CHECK-REBUILD
+
+client:
+  name: basic
+
+targets:
+  "": ["output"]
+  PREPARE: ["generated-input"]
+  
+commands:
+  # A command which consumes a generated input and reports it as a discovered dep.
+  C.consumer:
+    tool: shell
+    description: CONSUMER
+    inputs: ["<GENERATOR>"]
+    outputs: ["output"]
+    # We report the dependency twice just to cover the duplicate dependency scenario.
+    args:
+      "echo \"output: generated-input generated-input\" > output.d &&
+       cp generated-input output"
+    deps: output.d
+    deps-style: makefile
+
+  # A command which generates the discovered input.
+  #
+  # NOTE: The generated-input itself is intentionally never declared as an
+  # output here; this test is intentionally checking that we support a situation
+  # where the dependency edge is only valid due to some other explicit ordering
+  # in the build system, in this case the edge "C.consumer -> <GENERATOR>".
+  C.generator:
+    tool: shell
+    description: GENERATOR
+    inputs: ["input"]
+    outputs: ["<GENERATOR>"]
+    args: cp input generated-input

tests/Ninja/Build/tracing-scanning.ninja
@@ -5,7 +5,7 @@
 # RUN: cp %s %t.build/build.ninja
 # RUN: touch %t.build/input-1
 # RUN: touch %t.build/input-2
-# RUN: %{llbuild} ninja build --jobs 1 --chdir %t.build --trace %t.trace-out
+# RUN: %{llbuild} ninja build --jobs 1 --chdir %t.build
 
 # Check dependency scanning on rebuild.
 #
@@ -38,6 +38,6 @@
 rule CAT
      command = cat ${in} > $out
 
-build output: CAT input-2 input-1
+build output: CAT input-1 input-2
 
 default output

commit a325944d7cf362951ec02d6ff218fc2141292915

Optimize the rule dependency search query.
 - This adds an additional covering index for the rule dependency query, which
   is important for the null build case where our work largely amounts to
   populating and scanning the results.

 - This fixes a regression introduced in
   bcc3d1011eff6450d74f9b0bec913d17bf48efa2 -- the table primary key index was
   previously covering, but now is not because of the required ordinal
   parameter.

 - This change is unfortunate as there is a lot of redundancy in the tables, and
   this increases database size by 25% in one example, but it is also good for a
   ~10% win in null build time on a large graph.

 - <rdar://problem/32127527> Investigate optimizing rule dependency w/ ordinal lookup

lib/Core/SQLiteBuildDB.cpp
@@ -193,8 +193,21 @@ public:
         // Create an index to be used for efficiently looking up rule
         // information from a key.
         result = sqlite3_exec(
-               db, "CREATE UNIQUE INDEX rule_results_idx ON rule_results (key_id);",
-          nullptr, nullptr, &cError);
+            db, "CREATE UNIQUE INDEX rule_results_idx ON rule_results (key_id);",
+            nullptr, nullptr, &cError);
+      }
+
+      // Create a covering index for dependency queries.
+      //
+      // This is rather inefficient, because the rule_dependencies table is
+      // almost covering, but without this the dependency query is considerably
+      // more expensive -- this index alone is good for almost 10% on a null
+      // build of a large graph.
+      if (result == SQLITE_OK) {
+        result = sqlite3_exec(
+            db, ("CREATE UNIQUE INDEX rule_dependencies_idx "
+                 "ON rule_dependencies (rule_id, ordinal, key_id);"),
+            nullptr, nullptr, &cError);
       }
 
       // Sync changes to disk.