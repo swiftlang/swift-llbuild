f0890754a88440b9ad247fe9b1366588fb0d8790
Daniel Dunbar <daniel_dunbar@apple.com>
2017-10-27 15:00:43 -0700

    [gtest] Sync with LLVM's current gtest.
     - In particular, this fixes some warnings seen when using a more modern Clang.
     
     - <rdar://problem/35210645> 16 warnings in llbuild/utils/unittest/googletest files

--- cmake/modules/Utility.cmake
+++ cmake/modules/Utility.cmake
@@ -110,6 +110,7 @@ function(add_unittest test_suite test_name)
   endif ()
 
   set_property(TARGET ${test_name} APPEND PROPERTY COMPILE_DEFINITIONS GTEST_HAS_RTTI=0)
+  set_property(TARGET ${test_name} APPEND PROPERTY COMPILE_DEFINITIONS GTEST_NO_LLVM_RAW_OSTREAM=1)
 endfunction()
 
 # Compile swift sources to a dynamic framework.
