d4b3e267c6d8ce18a7c9c86685366201b9978641
David Bryson <dmbryson@apple.com>
2018-03-12 16:55:27 -0700

    Move C API versioning into Version.xcconfig (#239)

--- utils/Xcode/create-lit-site-cfg.sh
+++ utils/Xcode/create-lit-site-cfg.sh
@@ -27,6 +27,7 @@ sed < "${SRCROOT}/tests/lit.site.cfg.in" \
       > "${BUILT_PRODUCTS_DIR}/tests/lit.site.cfg" \
     -e "s=@LLBUILD_SRC_DIR@=${SRCROOT}=g" \
     -e "s=@LLBUILD_OBJ_DIR@=${BUILT_PRODUCTS_DIR}=g" \
+    -e "s=@LLBUILD_OUTPUT_DIR@=${BUILT_PRODUCTS_DIR}=g" \
     -e "s=@LLBUILD_TOOLS_DIR@=${BUILT_PRODUCTS_DIR}=g" \
     -e "s=@LLBUILD_LIBS_DIR@=${BUILT_PRODUCTS_DIR}=g" \
     -e "s=@FILECHECK_EXECUTABLE@=${FILECHECK}=g"
@@ -35,6 +36,7 @@ sed < "${SRCROOT}/tests/Unit/lit.site.cfg.in" \
       > "${BUILT_PRODUCTS_DIR}/tests/Unit/lit.site.cfg" \
     -e "s=@LLBUILD_SRC_DIR@=${SRCROOT}=g" \
     -e "s=@LLBUILD_OBJ_DIR@=${BUILT_PRODUCTS_DIR}=g" \
+    -e "s=@LLBUILD_OUTPUT_DIR@=${BUILT_PRODUCTS_DIR}=g" \
     -e "s=@LLBUILD_TOOLS_DIR@=${BUILT_PRODUCTS_DIR}=g" \
     -e "s=@LLBUILD_LIBS_DIR@=${BUILT_PRODUCTS_DIR}=g" \
     -e "s=@LLBUILD_BUILD_MODE@=.=g" \
