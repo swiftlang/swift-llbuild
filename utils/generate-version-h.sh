2e5738a8a79c4ed1be5eaccd00c9a0cf663ce1f5
Jake Petroules <jake.petroules@apple.com>
2018-03-27 15:09:31 -0700

    Prevent spurious rebuilds with SwiftPM builds of llbuild
    The generate-version-h.sh script was writing a new version.h file (thus
    updating the timestamp, thus triggering a change detection by the build
    system) every time it was run, regardless of whether the content had
    actually changed. Only update the file if doing so will actually change
    the content, in order to make null builds actually work again.

--- utils/generate-version-h.sh
+++ utils/generate-version-h.sh
@@ -17,8 +17,13 @@ if [ -z ${LLBUILD_C_API_VERSION+x} ]; then
     LLBUILD_C_API_VERSION=$(sed -n -e "s/.*LLBUILD_C_API_VERSION = \\([0-9][0-9]*\\).*/\\1/p" "${SRCROOT}/Xcode/Configs/Version.xcconfig")
 fi
 
-# Write out version.h from the template
-sed -e "s/\${LLBUILD_C_API_VERSION}/${LLBUILD_C_API_VERSION}/" "${SRCROOT}/products/libllbuild/include/llbuild/version.h.in" > "${SRCROOT}/products/libllbuild/include/llbuild/version.h"
+# Write out version.h from the template, but only update the header if it actually changed, in order to prevent spurious rebuilds
+llbuild_version_tmp="$(mktemp)"
+llbuild_version="${SRCROOT}/products/libllbuild/include/llbuild/version.h"
+sed -e "s/\${LLBUILD_C_API_VERSION}/${LLBUILD_C_API_VERSION}/" "${SRCROOT}/products/libllbuild/include/llbuild/version.h.in" > "$llbuild_version_tmp"
+if ! diff "$llbuild_version_tmp" "$llbuild_version" > /dev/null ; then
+    mv "$llbuild_version_tmp" "$llbuild_version"
+fi
 
 # For use with SwiftPM, output the conditional compilation flag to stdout
 echo "-Xswiftc -DLLBUILD_C_API_VERSION_${LLBUILD_C_API_VERSION}"
