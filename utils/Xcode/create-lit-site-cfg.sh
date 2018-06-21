bc76a09cf1e72801e2aa4b0038006acfd96b7748
David M. Bryson <dmbryson@apple.com>
2018-06-21 09:42:32 -0700

    [Xcode] Search Jenkins workspace for FileCheck

--- utils/Xcode/create-lit-site-cfg.sh
+++ utils/Xcode/create-lit-site-cfg.sh
@@ -12,13 +12,23 @@ PATH="$PATH:/usr/lib/llvm-3.7/bin"
 
 # If we have an included copy of FileCheck, use that.
 FILECHECK="${SRCROOT}/llbuild-test-tools/utils/Xcode/FileCheck"
+
+# If not locally, look for FileCheck in the path.
 if [ ! -f "${FILECHECK}" ]; then
-    # If not, look in the path.
     FILECHECK="$(which FileCheck || true)"
-    if [ -z "${FILECHECK}" ]; then
-        echo "$0: error: unable to find 'FileCheck' testing utility in path"
-        exit 1
+fi
+
+# If not in the path, search for it in the Jenkins ${WORKSPACE}
+if [ ! -f "${FILECHECK}" ]; then
+    if [ ! -z "${WORKSPACE}" ]; then
+        FILECHECK="$(find ${WORKSPACE} -name FileCheck -and -type f | tail -n 1)"
     fi
+fi
+
+# If we still haven't found FileCheck, bail
+if [ ! -f "${FILECHECK}" ]; then
+    echo "$0: error: unable to find 'FileCheck' testing utility"
+    exit 1
 fi
 
 mkdir -p "${BUILT_PRODUCTS_DIR}/tests/Unit"
