a79fcfc82724b58d413cb9e488e42d5fe781616e
Benjamin Herzog <benjamin_herzog@apple.com>
2019-07-26 16:41:43 +0200

    Added Swift interface for BuildKey
    This interface adds support for the specific parameters each BuildKey could have.
    
    rdar://50298914

---
+++ products/llbuildSwift/Internals.swift
@@ -0,0 +1,61 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+
+// This file contains Swift bindings for the llbuild C API.
+
+#if os(Linux)
+import Glibc
+#elseif os(Windows)
+import MSVCRT
+import WinSDK
+#else
+import Darwin.C
+#endif
+
+// We don't need this import if we're building
+// this file as part of the llbuild framework.
+#if !LLBUILD_FRAMEWORK
+import llbuild
+#endif
+
+/// Create a new `llb_data_t` instance containing an allocated copy of the given `bytes`.
+internal func copiedDataFromBytes(_ bytes: [UInt8]) -> llb_data_t {
+    // Create the data.
+    let buf = UnsafeMutableBufferPointer(start: UnsafeMutablePointer<UInt8>.allocate(capacity: bytes.count), count: bytes.count)
+
+    // Copy the data.
+    memcpy(buf.baseAddress!, UnsafePointer<UInt8>(bytes), buf.count)
+
+    // Fill in the result structure.
+    return llb_data_t(length: UInt64(buf.count), data: unsafeBitCast(buf.baseAddress, to: UnsafePointer<UInt8>.self))
+}
+
+// FIXME: We should eventually eliminate the need for this.
+internal func stringFromData(_ data: llb_data_t) -> String {
+    return String(decoding: UnsafeBufferPointer(start: data.data, count: Int(data.length)), as: Unicode.UTF8.self)
+}
+
+extension Array where Element == String {
+    internal func withCArrayOfStrings<T>(_ body: (UnsafePointer<UnsafePointer<CChar>>) -> T) -> T {
+        func appendPointer(_ index: Self.Index, to target: inout Array<UnsafePointer<CChar>>) -> T {
+            if index == self.endIndex {
+                return body(&target)
+            } else {
+                return self[index].withCString { cStringPtr in
+                    target.append(cStringPtr)
+                    return appendPointer(self.index(after: index), to: &target)
+                }
+            }
+        }
+
+        var elements = Array<UnsafePointer<CChar>>()
+        elements.reserveCapacity(self.count)
+
+        return appendPointer(self.startIndex, to: &elements)
+    }
+}
---
+++ unittests/Swift/BuildKeyTests.swift
@@ -0,0 +1,219 @@
+//
+//  BuildKeyTests.swift
+//  llbuildSwiftTests
+//
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import XCTest
+
+// The Swift package has llbuildSwift as module
+#if SWIFT_PACKAGE
+import llbuild
+import llbuildSwift
+#else
+import llbuild
+#endif
+
+private enum KeyTypeRepresentable {
+  case char(Character)
+  case number(UInt8)
+}
+
+extension KeyTypeRepresentable: ExpressibleByStringLiteral {
+  typealias StringLiteralType = String
+  
+  init(stringLiteral value: String) {
+    assert(value.count == 1)
+    self = .char(value.first!)
+  }
+}
+
+extension KeyTypeRepresentable: ExpressibleByIntegerLiteral {
+  init(integerLiteral value: UInt8) {
+    self = .number(value)
+  }
+}
+
+private extension Sequence where Element == KeyTypeRepresentable {
+  var asKey: KeyType {
+    compactMap { element in
+      switch element {
+      case .char(let char): return char.asciiValue
+      case .number(let num): return num
+      }
+    }
+  }
+}
+
+private extension String {
+  init?(_ key: KeyType) {
+    self.init(bytes: key, encoding: .utf8)
+  }
+  
+  var asKey: KeyType {
+    map { KeyTypeRepresentable.char($0) }.asKey
+  }
+}
+
+class BuildKeyTests: XCTestCase {
+
+  func testCommand() {
+    let command = BuildKey.Command(name: "foobar")
+    XCTAssertEqual(command.kind, .command)
+    XCTAssertEqual(command.name, "foobar")
+    XCTAssertEqual(command.keyData, "Cfoobar".asKey)
+    XCTAssertEqual(command, command)
+    XCTAssertEqual(command.description, "<BuildKey.Command name=foobar>")
+    XCTAssertNotEqual(command, BuildKey.Command(name: "foobar2"))
+  }
+  
+  func testCustomTask() {
+    let customTask = BuildKey.CustomTask(name: "foo", taskData: "bar")
+    XCTAssertEqual(customTask.kind, .customTask)
+    XCTAssertEqual(customTask.name, "foo")
+    XCTAssertEqual(customTask.taskData, "bar")
+    XCTAssertEqual(customTask.keyData, ["X", 3, 0, 0, 0, "f", "o", "o", "b", "a", "r"].asKey)
+    XCTAssertEqual(customTask, customTask)
+    XCTAssertNotEqual(customTask, BuildKey.CustomTask(name: "foo2", taskData: "bar2"))
+  }
+
+  func testDirectoryContents() {
+    let directoryContents = BuildKey.DirectoryContents(path: "/foo/bar")
+    XCTAssertEqual(directoryContents.kind, .directoryContents)
+    XCTAssertEqual(directoryContents.path, "/foo/bar")
+    XCTAssertEqual(directoryContents.keyData, "D/foo/bar".asKey)
+    XCTAssertEqual(directoryContents, directoryContents)
+    XCTAssertNotEqual(directoryContents, BuildKey.DirectoryContents(path: "/foo/bar2"))
+  }
+
+  func testFilteredDirectoryContents() {
+    let filteredDirectoryContents = BuildKey.FilteredDirectoryContents(path: "/foo/bar", filters: ["jpg", "png"])
+    XCTAssertEqual(filteredDirectoryContents.kind, .filteredDirectoryContents)
+    XCTAssertEqual(filteredDirectoryContents.path, "/foo/bar")
+    XCTAssertEqual(filteredDirectoryContents.filters, ["jpg", "png"])
+    XCTAssertEqual(filteredDirectoryContents.keyData, ["d"].asKey + [8, 0, 0, 0].asKey + "/foo/bar".asKey + [8, 0, 0, 0, 0, 0, 0, 0].asKey + "jpg\0png\0".asKey)
+    XCTAssertEqual(filteredDirectoryContents, filteredDirectoryContents)
+    XCTAssertNotEqual(filteredDirectoryContents, BuildKey.FilteredDirectoryContents(path: "/foo/bar2", filters: ["jpg"]))
+  }
+
+  func testDirectoryTreeSignature() {
+    let directoryTreeSignature = BuildKey.DirectoryTreeSignature(path: "/foo/bar", filters: ["jpg", "png"])
+    XCTAssertEqual(directoryTreeSignature.kind, .directoryTreeSignature)
+    XCTAssertEqual(directoryTreeSignature.path, "/foo/bar")
+    XCTAssertEqual(directoryTreeSignature.filters, ["jpg", "png"])
+    XCTAssertEqual(directoryTreeSignature.keyData, ["S"].asKey + [8, 0, 0, 0].asKey + "/foo/bar".asKey + [8, 0, 0, 0, 0, 0, 0, 0].asKey + "jpg\0png\0".asKey)
+    XCTAssertEqual(directoryTreeSignature, directoryTreeSignature)
+    XCTAssertNotEqual(directoryTreeSignature, BuildKey.DirectoryTreeSignature(path: "/foo/bar2", filters: ["jpg"]))
+  }
+
+  func testDirectoryTreeStructureSignature() {
+    let directoryTreeStructureSignature = BuildKey.DirectoryTreeStructureSignature(path: "/foo/bar")
+    XCTAssertEqual(directoryTreeStructureSignature.kind, .directoryTreeStructureSignature)
+    XCTAssertEqual(directoryTreeStructureSignature.path, "/foo/bar")
+    XCTAssertEqual(directoryTreeStructureSignature.keyData, "s/foo/bar".asKey)
+    XCTAssertEqual(directoryTreeStructureSignature, directoryTreeStructureSignature)
+    XCTAssertNotEqual(directoryTreeStructureSignature, BuildKey.DirectoryTreeStructureSignature(path: "/foo/bar2"))
+  }
+
+  func testNode() {
+    let node = BuildKey.Node(path: "/foo/bar")
+    XCTAssertEqual(node.kind, .node)
+    XCTAssertEqual(node.path, "/foo/bar")
+    XCTAssertEqual(node.keyData, "N/foo/bar".asKey)
+    XCTAssertEqual(node, node)
+    XCTAssertNotEqual(node, BuildKey.Node(path: "/foo/bar2"))
+  }
+
+  func testStat() {
+    let stat = BuildKey.Stat(path: "/foo/bar")
+    XCTAssertEqual(stat.kind, .stat)
+    XCTAssertEqual(stat.path, "/foo/bar")
+    XCTAssertEqual(stat.keyData, "I/foo/bar".asKey)
+    XCTAssertEqual(stat, stat)
+    XCTAssertNotEqual(stat, BuildKey.Stat(path: "/foo/bar2"))
+  }
+
+  func testTarget() {
+    let target = BuildKey.Target(name: "foobar")
+    XCTAssertEqual(target.kind, .target)
+    XCTAssertEqual(target.name, "foobar")
+    XCTAssertEqual(target.keyData, "Tfoobar".asKey)
+    XCTAssertEqual(target, target)
+    XCTAssertNotEqual(target, BuildKey.Target(name: "foobar2"))
+  }
+
+  func testConstruct() throws {
+    // This test creates a raw llb_build_key_t and constructs a BuildKey from the given BuildKey instance
+    func test<T: BuildKey>(_ instance: T, file: StaticString = #file, line: UInt = #line, _ expectation: (T) throws -> Void) throws {
+      let keyData = Array(instance.keyData.dropFirst())
+      try keyData.withUnsafeBufferPointer { ptr in
+        let data = llb_data_t(length: UInt64(keyData.count), data: ptr.baseAddress)
+        let internalKey = llb_build_key_t(kind: instance.kind, key: data)
+        guard let key = BuildKey.construct(key: internalKey) as? T else {
+          XCTFail("Expected to be able to construct a build key from \(internalKey), but BuildKey.construct(key:) returned nil.", file: file, line: line)
+          return
+        }
+        try expectation(key)
+      }
+    }
+
+    try test(BuildKey.Command(name: "foobar")) {
+      XCTAssertEqual($0.kind, .command)
+      XCTAssertEqual($0.name, "foobar")
+      XCTAssertEqual($0.keyData, "Cfoobar".asKey)
+    }
+
+    try test(BuildKey.CustomTask(name: "foo", taskData: "bar")) {
+      XCTAssertEqual($0.kind, .customTask)
+      XCTAssertEqual($0.name, "foo")
+      XCTAssertEqual($0.taskData, "bar")
+      XCTAssertEqual($0.keyData, ["X", 3, 0, 0, 0, "f", "o", "o", "b", "a", "r"].asKey)
+    }
+    
+    try test(BuildKey.DirectoryContents(path: "/foo/bar")) {
+      XCTAssertEqual($0.kind, .directoryContents)
+      XCTAssertEqual($0.path, "/foo/bar")
+      XCTAssertEqual($0.keyData, "D/foo/bar".asKey)
+    }
+    
+    try test(BuildKey.FilteredDirectoryContents(path: "/foo/bar", filters: ["jpg", "png"])) {
+      XCTAssertEqual($0.kind, .filteredDirectoryContents)
+      XCTAssertEqual($0.path, "/foo/bar")
+      XCTAssertEqual($0.filters, ["jpg", "png"])
+      XCTAssertEqual($0.keyData, ["d"].asKey + [8, 0, 0, 0].asKey + "/foo/bar".asKey + [8, 0, 0, 0, 0, 0, 0, 0].asKey + "jpg\0png\0".asKey)
+    }
+    
+    try test(BuildKey.DirectoryTreeSignature(path: "/foo/bar", filters: ["jpg", "png"])) {
+      XCTAssertEqual($0.kind, .directoryTreeSignature)
+      XCTAssertEqual($0.path, "/foo/bar")
+      XCTAssertEqual($0.filters, ["jpg", "png"])
+      XCTAssertEqual($0.keyData, ["S"].asKey + [8, 0, 0, 0].asKey + "/foo/bar".asKey + [8, 0, 0, 0, 0, 0, 0, 0].asKey + "jpg\0png\0".asKey)
+    }
+    
+    try test(BuildKey.DirectoryTreeStructureSignature(path: "/foo/bar")) {
+      XCTAssertEqual($0.kind, .directoryTreeStructureSignature)
+      XCTAssertEqual($0.path, "/foo/bar")
+      XCTAssertEqual($0.keyData, "s/foo/bar".asKey)
+    }
+    
+    try test(BuildKey.Node(path: "/foo/bar")) {
+      XCTAssertEqual($0.kind, .node)
+      XCTAssertEqual($0.path, "/foo/bar")
+      XCTAssertEqual($0.keyData, "N/foo/bar".asKey)
+    }
+    
+    try test(BuildKey.Stat(path: "/foo/bar")) {
+      XCTAssertEqual($0.kind, .stat)
+      XCTAssertEqual($0.path, "/foo/bar")
+      XCTAssertEqual($0.keyData, "I/foo/bar".asKey)
+    }
+    
+    try test(BuildKey.Target(name: "foobar")) {
+      XCTAssertEqual($0.kind, .target)
+      XCTAssertEqual($0.name, "foobar")
+      XCTAssertEqual($0.keyData, "Tfoobar".asKey)
+    }
+  }
+  
+}
