757e43825963e0311b51fbf008bdd3e15ed76e59
Benjamin Herzog <benjamin_herzog@apple.com>
2019-08-08 17:31:13 +0200

    Added timing information to the build database
    A new Clock class gives access to static methods for getting global timestamps in seconds since the clock's epoch time.
    The timing information (start and end) is stored in the SQLite database in the rule results table. That defines a new schema version.
    
    rdar://52892354

---
+++ include/llbuild/Basic/Clock.h
@@ -0,0 +1,42 @@
+//===- Clock.h --------------------------------------------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLBUILD_BASIC_CLOCK_h
+#define LLBUILD_BASIC_CLOCK_h
+
+#include <algorithm>
+#include <chrono>
+
+namespace llbuild {
+namespace basic {
+
+class Clock {
+public:
+  /// A timestamp is the number of seconds since the clock's epoch time.
+  typedef double Timestamp;
+  
+  Clock() LLBUILD_DELETED_FUNCTION;
+  
+  /// Returns a global timestamp that represents the current time in seconds since a reference data.
+  /// *NOTE*: This function uses a monotonic clock, so don't compare between systems.
+  inline static Timestamp now() {
+    // steady_clock is monotonic
+    auto now = std::chrono::steady_clock::now();
+    std::chrono::duration<double> difference = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch());
+    return difference.count();
+  }
+};
+
+}
+}
+
+#endif
